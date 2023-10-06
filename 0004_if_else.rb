2439e56b35eaee58fb3cc6e5229d45cb6ede7548
awan <awan@awancoder.com>
2023-10-06 20:30:24 +0700

    if statment

--- 0004_if_else.rb
+++
@@ -1,7 +0,0 @@
-x = 15
-
-if x > 15
-    puts "lebih besar dari 15"
-else
-    puts "lebih kecil sama dengan 15"
-end
---
+++ 0004_if_statement.rb
@@ -0,0 +1,23 @@
+x = 15
+
+# if
+if x == 15
+    puts " x itu 15"
+end
+
+# if else
+if x > 15
+    puts "lebih besar dari 15"
+else
+    puts "lebih kecil sama dengan 15"
+end
+
+# if elsif else
+
+if x < 10
+    puts "kurang dari 10"
+elsif x = 15
+    puts "x sama dengan 15"
+else
+    puts "x tidak di handle"
+end
