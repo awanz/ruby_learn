c0bb9c6ef5649f356e7a5674389f1a6f4c551b33
awan <awan@awancoder.com>
2023-10-07 09:41:35 +0700

    case statement

--- 0006_case_statement.rb
+++ 0006_case_statement.rb
@@ -1,12 +1,38 @@
-x = 3
+# case statement text
+text = "awan"
 
-case x
-when 1
-    puts "x is 1"
-when 15
-    puts "x is 15"
-when 20
-    puts "x is 20"
+case text
+when "awan"
+    puts text + " here"
+when "dina"
+    puts "dina"
 else
-    puts "default"
+    puts "tidak terdefinisi"
+end
+
+# case statement number
+number = 24
+
+case number
+when 1
+    puts "numb is 1"
+when 2..21
+    puts "numb antara 2 - 21"
+when 22..25
+    puts "numb antara 22 - 25"
+when 25..50
+    puts "numb antara 25 - 50"
+else
+    puts "tidak terdefinisi"
+end
+
+str = "!"
+
+case 
+when str.match(/\d/)
+    puts "string mengandung angka"
+when str.match(/[a-zA-Z]/)
+    puts "string mengandung huruf"
+else 
+    puts "string tidak ada huruf atau angka"
 end
