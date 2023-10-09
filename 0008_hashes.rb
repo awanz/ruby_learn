224894794dd32c96748636c51c58c6d8e00f9ce7
awan <awan@awancoder.com>
2023-10-09 21:56:32 +0700

    hashes

---
+++ 0008_hashes.rb
@@ -0,0 +1,14 @@
+color = {   
+    "Rose" => "red",   
+    "Lily" => "purple",   
+    "Marigold" => "yellow",   
+    "Jasmine" => "white"   
+}
+puts color
+color['Tulip'] = "pink"   
+puts color
+
+puts color['Rose']   
+puts color['Lily']   
+puts color['Marigold']   
+puts color['Jasmine']  
